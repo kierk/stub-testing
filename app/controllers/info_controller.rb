1da8e2798c5cf1c4e15ad1218829aab094013b8c
Drew Pellitieri <kierke@bu.edu>
2016-09-07 10:46:50 -0400

    changed path to correct permissions issue

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,6 +1,6 @@
 class InfoController < ApplicationController
   def index
-    file = File.read('/data/info.json')
+    file = File.read('/opt/app-root/data/info.json')
     data_hash = JSON.parse(file)
     @title = data_hash['title']
     @description = data_hash['description']
